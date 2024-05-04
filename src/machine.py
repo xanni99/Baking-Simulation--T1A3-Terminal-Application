a6d3606921161e9d7f3022ea01fd4dbd2634d926
xanni99 <xanni99.m@hotmail.com>
2024-05-04 14:36:56 +1000

    created Machine Class and gave initial ingredient levels

---
+++ src/machine.py
@@ -0,0 +1,12 @@
+class Machine:
+    def __init__(self, eggs: int=6, milk: int=500, butter: int=500, flour: int=500, sugar: int=500, chocolate: int=300, vanilla: int=50, water: int=250, soap: int= 50):
+        self.eggs = eggs
+        self.milk = milk
+        self.butter = butter
+        self.flour = flour
+        self.sugar = sugar
+        self.chocolate = chocolate
+        self.vanilla = vanilla
+        self.water = water
+        self.soap = soap
+        
