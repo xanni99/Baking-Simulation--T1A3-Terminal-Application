10c2c6ad6ed6ac04f0fa3d8c1c601a2adf559cec
xanni99 <xanni99.m@hotmail.com>
2024-05-04 14:17:54 +1000

    added user interface (display messages)

---
+++ src/user_interface.py
@@ -0,0 +1,12 @@
+def welcome_message():
+    print("Welcome to the Baker 3000. I can't wait to bake you a tasty treat!")
+
+def goodbye_message():
+    print("Thank you for using the Baker 3000. Have a nice day!")
+
+def baked_goods():
+    print("1. Chocolate Chip Cookies")
+    print("2. Vanilla Cupcakes")
+    print("3. Brownies")
+    print("4. Chocolate Muffins")
+
