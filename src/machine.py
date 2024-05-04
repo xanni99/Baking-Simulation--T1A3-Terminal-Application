33e18332a92efac082ec6a3002a4a1fa1ee533be
xanni99 <xanni99.m@hotmail.com>
2024-05-04 16:43:02 +1000

    added match cases for refill function

--- src/machine.py
+++ src/machine.py
@@ -9,4 +9,99 @@ class Machine:
         self.vanilla = vanilla
         self.water = water
         self.soap = soap
+
+    def refill_ingredients(self):
+        print("What ingredient would you like to refill?\n")
+        print(f"[1] Eggs, current amount = {self.eggs}/6\n")
+        print(f"[2] Milk, current amount = {self.milk}/500\n")
+        print(f"[3] Butter, current amount = {self.butter}/500\n")
+        print(f"[4] Flour, current amount = {self.flour}/500\n")
+        print(f"[5] Sugar, current amount = {self.sugar}/500\n")
+        print(f"[6] Chocolate, current amount = {self.chocolate}/300\n")
+        print(f"[7] Vanilla, current amount = {self.vanilla}/50\n")
+        print(f"[8] Water, current amount = {self.water}/250\n")
+        print(f"[9] Soap, current amount = {self.soap}/50\n")
+
+        try:
+            ingredient = int(input("Enter the number of the ingredient you would like to refill: "))
+            if ingredient < 1 or ingredient > 9:
+                raise ValueError
+
+        except ValueError:
+            print("\n Please enter a number between 1 and 9\n")
+            self.refill_ingredients()
+            
+        match ingredient:
+            case 1:
+                refill_amount = int(input("\nHow many eggs would you like to add?"))
+                if refill_amount + self.eggs > 6:
+                    print("\nI cannot store more than 6 eggs!\n")
+                    self.refill_ingredients()
+                self.eggs += refill_amount
+                print(f"I now have {self.eggs} eggs!")
+            case 2:
+                refill_amount = int(input("\nHow much milk(ml) would you like to add?"))
+                if refill_amount + self.milk > 500:
+                    print("\nI cannot store more than 500ml milk!\n")
+                    self.refill_ingredients()
+                self.milk += refill_amount
+                print(f"I now have {self.milk}mls of milk!")
+            case 3:
+                refill_amount = int(input("\nHow much butter(g) would you like to add?"))
+                if refill_amount + self.butter > 500:
+                    print("\nI cannot store more than 500g butter!\n")
+                    self.refill_ingredients()
+                self.butter += refill_amount
+                print(f"I now have {self.butter}g of butter!")
+            case 4:
+                refill_amount = int(input("\nHow much flour(g) would you like to add?"))
+                if refill_amount + self.flour > 500:
+                    print("\nI cannot store more than 500g flour!\n")
+                    self.refill_ingredients()
+                self.flour += refill_amount
+                print(f"I now have {self.flour}g of flour!")
+            case 5:
+                refill_amount = int(input("\nHow much sugar(g) would you like to add?"))
+                if refill_amount + self.sugar > 500:
+                    print("\nYou cannot add more than 500g sugar!\n")
+                    self.refill_ingredients()
+                self.sugar += refill_amount
+                print(f"You now have {self.sugar} sugar!")
+            case 6:
+                refill_amount = int(input("\nHow much chocolate(g) would you like to add?"))
+                if refill_amount + self.chocolate > 300:
+                    print("\nI cannot store more than 300g chocolate!\n")
+                    self.refill_ingredients()
+                self.chocolate += refill_amount
+                print(f"I now have {self.chocolate}g of chocolate!")
+            case 7:
+                refill_amount = int(input("\nHow much vanilla(ml) would you like to add?"))
+                if refill_amount + self.vanilla > 50:
+                    print("\nI cannot store more than 50ml vanilla!\n")
+                    self.refill_ingredients()
+                self.vanilla += refill_amount
+                print(f"I now have {self.vanilla}mls of vanilla!")
+            case 8:
+                refill_amount = int(input("\nHow much water(ml) would you like to add?"))
+                if refill_amount + self.water > 250:
+                    print("\nI cannot store more than 250ml water!\n")
+                    self.refill_ingredients()
+                self.water += refill_amount
+                print(f"I now have {self.water}mls of water!")
+            case 9:
+                refill_amount = int(input("\nHow much soap(g) would you like to add?"))
+                if refill_amount + self.soap > 50:
+                    print("\nI cannot store more than 50g soap!\n")
+                    self.refill_ingredients()
+                self.soap += refill_amount
+                print(f"I now have {self.soap}g of soap!")
+            case _:
+                print("\n Please enter a number between 1 and 9\n")
+                self.refill_ingredients()
+
+test = Machine()
+
+test.refill_ingredients ()
+
+
         
