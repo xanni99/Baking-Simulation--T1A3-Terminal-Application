94187c7c35190da1d511eb21e6ca96bab336c583
xanni99 <xanni99.m@hotmail.com>
2024-05-08 14:02:22 +1000

    added some comments to explain functions

--- src/test_baker3000.py
+++ src/test_baker3000.py
@@ -16,17 +16,15 @@ def test_load_existing_json_file():
     "soap": 50
 }
 
-#Testing Ingredient will be listed in right colour depending on current ingredient level
-#Test that if level of "eggs" currently in machine = 5 it should be printed in green (as it is above 75% full and is enough to be used in any of the recipes) 
-#Test that if level of "milk" currently in machine = 350 it should be printed in yellow (as it is above 55% full but below 75% and is enough to be used in at least 1 of the recipes) 
-#Test that if level of "butter" currently in machine = 210 it should be printed in red (as it is below 55% and is not enough to be used in any of the recipes)
-class TestColourListed:
 
+
+class TestColourListed:
+#Testing Ingredient will be listed in right colour depending on current ingredient level
     def mock_ingredients(self):
         return {
-            "eggs": 5,
-            "milk": 350,
-            "butter": 210,
+            "eggs": 5, #83% of Max Capacity
+            "milk": 350, #58% of Max Capacity
+            "butter": 210, #35% of Max Capacity
         }
 
     def max_ingredients(self):
@@ -37,6 +35,9 @@ class TestColourListed:
         }
 
     def test_correct_colours(self, capsys):
+        #If level of "eggs" currently in machine = 5 it should be printed in green (as it is above 75% full) and is enough to be used in any of the recipes) 
+        #If level of "milk" currently in machine = 350 it should be printed in yellow (as it is above 55% full but below 75%) and is enough to be used in at least 1 of the recipes) 
+        #If level of "butter" currently in machine = 210 it should be printed in red (as it is below 55%) and is not enough to be used in any of the recipes)
         baker3000 = Machine()
         baker3000.ingredients = self.mock_ingredients()
         baker3000.list_ingredients()
