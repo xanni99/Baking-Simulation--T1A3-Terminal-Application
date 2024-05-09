53d0a218c6639a02ec2fc6defa0fafc948794a58
xanni99 <xanni99.m@hotmail.com>
2024-05-09 13:29:51 +1000

    added docstrings to user_interface.py

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,15 +1,21 @@
 import os
 
+    """This module contains various functions that relate to the user interface of the machine"""
+
 def clear():
+    """Clears the terminal screen"""
     os.system("cls" if os.name == "nt" else "clear")
     
 def welcome_message():
+    """Displays Welcome Message"""
     print("Welcome to the Baker 3000. I can't wait to bake you a tasty treat!")
 
 def goodbye_message():
+    """Displays Goodbye Message"""
     print("Thank you for using the Baker 3000. Have a nice day!")
 
 def user_menu():
+    """Displays User Menu"""
     clear()
     print("What would you like to do?\n \nPlease select a number from the list below\n")
     print("1. Bake a Treat!")
