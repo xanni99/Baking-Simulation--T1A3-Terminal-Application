e4b4dc5a37a11763df6367eeaf147159dc554bae
xanni99 <xanni99.m@hotmail.com>
2024-05-09 11:18:35 +1000

    put refill_ingredients function into a while loop so that the user can refill more than one ingredient before returning to main menu

--- src/baker3000.py
+++ src/baker3000.py
@@ -49,44 +49,61 @@ class Machine:
         print(f"{Fore.RED} Red = Not enough of ingredient to be used in ANY recipes - You need to refill")
 
     def refill_ingredients(self):
-        self.list_ingredients()
-        try:
-            ingredient_to_refill = input("\nPlease enter the name of the ingredient you would like to refill\n").lower()
-            max_quantity = self.max_quantities.get(ingredient_to_refill)
-            if max_quantity is not None:
-                current_quantity = self.ingredients.get(ingredient_to_refill)
-                refill_amount = int(input(f"\nHow much/many {ingredient_to_refill.capitalize()} would you like to refill? I currently have {self.ingredients[ingredient_to_refill]} out of {self.max_quantities.get(ingredient_to_refill)}\n"))
-                if refill_amount >= 0:
-                    if current_quantity + refill_amount <= max_quantity:
-                        self.ingredients[ingredient_to_refill] += refill_amount
-                        clear()
-                        print("Refilling... Please Wait...")
-                        time.sleep(3)
-                        clear()
-                        print(f"I now have {self.ingredients.get(ingredient_to_refill)} units of {ingredient_to_refill.capitalize()}\n")
-                        print("Returning to Main Menu...")
+        while True:
+            self.list_ingredients()
+            print("\nWhat would you like to do?")
+            print("\n[1] Refill ingredient")
+            print("[2] Return to Main Menu")
+            decision = input("\nEnter the number of your choice: ")
+            if decision == '1':
+                clear()
+                self.list_ingredients()
+                try:
+                    ingredient_to_refill = input("\nPlease enter the name of the ingredient you would like to refill\n").lower()
+                    max_quantity = self.max_quantities.get(ingredient_to_refill)
+                    clear()
+                    if max_quantity is not None:
+                        current_quantity = self.ingredients.get(ingredient_to_refill)
+                        refill_amount = int(input(f"\nHow much/many {ingredient_to_refill.capitalize()} would you like to refill? I currently have {self.ingredients[ingredient_to_refill]} out of {self.max_quantities.get(ingredient_to_refill)}\n"))
+                        if refill_amount >= 0:
+                            if current_quantity + refill_amount <= max_quantity:
+                                self.ingredients[ingredient_to_refill] += refill_amount
+                                clear()
+                                print("Refilling... Please Wait...")
+                                time.sleep(3)
+                                clear()
+                                print(f"I now have {self.ingredients.get(ingredient_to_refill)} units of {ingredient_to_refill.capitalize()}\n")
+                                time.sleep(3)
+                                clear()
+                            else:
+                                print(f"I cannot store more than {max_quantity} units of {ingredient_to_refill.capitalize()}, please try again\n")
+                                time.sleep(2)
+                                clear()
+                                self.refill_ingredients()
+                        else:
+                            print("Please enter a positive number greater than 0")
+                            time.sleep(2)
+                            clear()
+                            self.refill_ingredients()
                     else:
-                        print(f"I cannot store more than {max_quantity} units of {ingredient_to_refill.capitalize()}, please try again\n")
+                        print(f"\n{ingredient_to_refill} is not a valid ingredient, please try again")
                         time.sleep(2)
                         clear()
                         self.refill_ingredients()
-                else:
-                    print("Please enter a positive number greater than 0")
+                except ValueError:
+                    print("\n -- Invalid input -- I can only accept numbers, please try again\n")
                     time.sleep(2)
                     clear()
                     self.refill_ingredients()
+            elif decision == '2':
+                clear()
+                print("Returning to Main Menu...")
+                break
             else:
-                print(f"\n{ingredient_to_refill} is not a valid ingredient, please try again")
+                print("\n -- Invalid input -- Please enter either '1' or '2'\n")
                 time.sleep(2)
                 clear()
-                self.refill_ingredients()
-        except ValueError:
-            print("\n -- Invalid input -- I can only accept numbers, please try again\n")
-            time.sleep(2)
-            clear()
-            self.refill_ingredients()
-        finally:
-            self.save_ingredients()
+        self.save_ingredients()
 
     def display_treat(self,choice):
         if choice == '1':
